9eb3b1eb15f202247ffbfed3f45354c08a44b50c
Wartha Sensei <59157041+WarthaDev@users.noreply.github.com>
2023-08-09 01:32:48 +0800

    Add files via upload

--- costume-js/spinner-loading.js
+++ costume-js/spinner-loading.js
@@ -8,3 +8,14 @@ window.addEventListener('load', function () {
     // menambahkan kelas "d-none" ke spinner sehingga spinner bisa disembunyikan
     spinner.classList.add('d-none');
 });
+
+window.addEventListener('load', function () {
+    // mengambil elemen spinner
+    const spinner = document.getElementById('my-spinner');
+    // mengambil elemen konten
+    const content = document.getElementById('banner');
+    // menghapus kelas "d-none" dari konten sehingga konten bisa ditampilkan
+    content.classList.remove('d-none');
+    // menambahkan kelas "d-none" ke spinner sehingga spinner bisa disembunyikan
+    spinner.classList.add('d-none');
+});
